b024ce099cb1a5be3772e54ecfc4632eed346305
Zepco <dev@windy-kielce.pl>
2023-08-18 19:32:04 +0000

    Pogoda - uproszczona obsługa błędów

--- commands/misc/weather.js
+++ commands/misc/weather.js
@@ -12,38 +12,46 @@ module.exports = {
   async execute(interaction) {
     const cityName = interaction.options.getString("miasto");
 
-    const res = await axios
-      .get("http://api.openweathermap.org/data/2.5/weather", {
+    const weatherEmbed = new EmbedBuilder()
+
+    await axios
+      .get("http://api.openweathermap.org/data/2.5/weather", {  // adres zapytania do serwera pogody
         params: {
-          q: `${cityName},PL`,
-          appid: openWeatherMapKey,
-          lang: "PL",
-          units: "metric",
+          q: `${cityName},PL`,        // nazwa miasta w Polsce
+          appid: openWeatherMapKey,   // klucz dp API OpenWeatherMap - pobrany z pliku config.json
+          lang: "PL",                 // język polski
+          units: "metric",            // jednostki metryczne
         },
       })
-      .then((res) => {
-        return res.data;
+      .then((res) => {  // jeśli wszystko przebiegło pomyślnie
+        weatherEmbed
+          .setColor(0x00ffff)
+          .setTitle('Pogoda')
+          .setDescription(`Pogoda dla miasta ${cityName}`)
+          .setThumbnail(`https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`)
+          .addFields(
+            { name: 'Informacje ogólne', value: res.weather[0].description },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Temperatura', value: res.main.temp + ' \u00B0C', inline: true },
+            { name: 'Temp. odczuwalna', value: res.main.feels_like + ' \u00B0C', inline: true },
+            { name: 'Ciśnienie', value: res.main.pressure + ' hPa', inline: true },
+            { name: 'Wilgotność', value: res.main.humidity + '%', inline: true },
+            { name: 'Zachmurzenie', value: res.clouds.all + '%', inline: true},
+            { name: 'Prędk. wiatru', value: res.wind.speed + ' km/h', inline: true},
+          )
+          .setFooter({text: 'Pogoda w oparciu o OpenWeatherMap'})
+          .setTimestamp();
+      })
+      .catch((error) => { // Przechwyć błędy
+        if (error.response) { // Jeśli błąd wysyłany przez serwer API
+          weatherEmbed
+            .setTitle('Błąd')
+            .setDescription(error.response.data.message)
+            .setColor(0xFF0000);
+        }
       });
 
     // console.log(res);
-
-    const weatherEmbed = new EmbedBuilder()
-    .setColor(0x00ffff)
-    .setTitle('Pogoda')
-    .setDescription(`Pogoda dla miasta ${cityName}`)
-    .setThumbnail(`https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`)
-    .addFields(
-      { name: 'Informacje ogólne', value: res.weather[0].description },
-      { name: '\u200B', value: '\u200B' },
-      { name: 'Temperatura', value: res.main.temp + ' \u00B0C', inline: true },
-      { name: 'Temp. odczuwalna', value: res.main.feels_like + ' \u00B0C', inline: true },
-      { name: 'Ciśnienie', value: res.main.pressure + ' hPa', inline: true },
-      { name: 'Wilgotność', value: res.main.humidity + '%', inline: true },
-      { name: 'Zachmurzenie', value: res.clouds.all + '%', inline: true},
-      { name: 'Prędk. wiatru', value: res.wind.speed + ' km/h', inline: true},
-    )
-    .setFooter({text: 'Pogoda w oparciu o OpenWeatherMap'})
-    .setTimestamp();
   
     return interaction.reply(
       { embeds: [weatherEmbed] }
