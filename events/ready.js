32a9a634d9213a7f753617eb7c7559dd1664b6eb
Zepco <dev@windy-kielce.pl>
2023-08-27 10:28:17 +0000

    komentarze

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+	name: Events.ClientReady,	// Zdarzenie na jakie ma reagować
+	once: true,					// Czy wykonać komendę tylko raz? true - tak, false - wykonuj za każdym razem gdy wystąpi dane zdarzenie
+	execute(client) {			// Funkcja która się wykona za każdym razem kiedy zdarzenie zostanie wywołane
+		console.log(`Bot gotowy! Zalogowany jako ${client.user.tag}`);	// Wyświetla na konsoli info o zalogowaniu bota
 	},
 };
--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
+/*** Przeszukiwanie folderu 'commands' w poszukiwaniu komend ***/
+
+client.commands = new Collection();						// Kolekcja "worek" do przechowywania informacji o komendach
+const foldersPath = path.join(__dirname, 'commands');	// Ścieżka do komend
+const commandFolders = fs.readdirSync(foldersPath);		// Wczytaj wszystkie foldery w katalogu commands
+
+for (const folder of commandFolders) {	// Przejrzyj wszystkie podfoldery
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); // Szukaj tam plików z rozszerzeniem .js
+	for (const file of commandFiles) {														// Przejrzyj wszystkie pliki z danego podfolderu
 		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		const command = require(filePath);													// Pobierz komendę z danego pliku
+		if ('data' in command && 'execute' in command) {									// Jeśli posiada dane i funkcję
+			client.commands.set(command.data.name, command); 								// Dopisz je do listy komend
+		} else {																			// W przeciwnym wypadku
+			console.log(`[OSTRZEŻENIE] Komenda w pliku ${filePath} nie posiada wymaganego parametru "data" lub "execute".`); // Wyświetl ostrzeżenie
 		}
 	}
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
+/*** Przeszukiwanie folderu 'events' w poszukiwaniu pozostałych działań bota ***/
+
+const eventsPath = path.join(__dirname, 'events');											// Ścieżka do "zdarzeń"
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));			// Szukaj plików z rozszerzeniem .js
+
+for (const file of eventFiles) {															// Przejrzyj wszystkie znalezione pliki
 	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+	const event = require(filePath);														// Otwórz je pobierz "zdarzenia"
+	if (event.once) {																		// Jeśli zdarzenie ma zmienną once ustawioną na true
+		client.once(event.name, (...args) => event.execute(...args));						// Dodaj "zdarzenie" które odpala się tylko raz
+	} else {																				// W przeciwnym wypadku
+		client.on(event.name, (...args) => event.execute(...args));							// Dodaj "zdarzenie" które odpala się za każdym razem
 	}
 }
 
+
+/*** Jak już wszystkie parametry bota są ustawione, odpalamy go! ***/
 client.login(token);
